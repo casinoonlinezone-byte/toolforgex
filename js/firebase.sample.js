a9f478037e2045ac49506d4dde7665f64a613049
casinoonlinezone-byte <casinoonlinezone@gmail.com>
2025-11-21 07:50:00 +0500

    Update firebase.sample.js

--- js/firebase.sample.js
+++ js/firebase.sample.js
@@ -1,10 +1,10 @@
-﻿export const firebaseConfig = {
-  apiKey: \"YOUR_API_KEY\",
-  authDomain: \"YOUR_PROJECT.firebaseapp.com\",
-  projectId: \"YOUR_PROJECT_ID\",
-  storageBucket: \"YOUR_PROJECT.appspot.com\",
-  messagingSenderId: \"YOUR_SENDER_ID\",
-  appId: \"YOUR_APP_ID\",
-  measurementId: \"YOUR_MEASUREMENT_ID\"
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ5-b_IVSdxYN66XH14WpRSCU6Toyh2FE",
+  authDomain: "earnify-pro-4d01a.firebaseapp.com",
+  databaseURL: "https://earnify-pro-4d01a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "earnify-pro-4d01a",
+  storageBucket: "earnify-pro-4d01a.firebasestorage.app",
+  messagingSenderId: "492457671396",
+  appId: "1:492457671396:web:1b333f3a5943f863a2caf1",
+  measurementId: "G-GMVQ6DJMMF"
 };
-// IMPORTANT: copy this file to js/firebase.js with your real values, or use Vercel env vars.
